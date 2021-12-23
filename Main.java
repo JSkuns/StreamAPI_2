0824f0b9ebb192b9ecd974dde1a9d55f6a62bb8f
Skuns <48394979+JSkuns@users.noreply.github.com>
2021-12-23 14:13:22 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        new SolutionViaStream(); // решение с использованием Stream API
+        System.out.println();
+
+        new SolutionViaCollection(); // решение с использованием Collection API
+        System.out.println();
+
+        new SolutionViaArray(); // решение без использования Stream API и Collection API
+
+    }
+}
